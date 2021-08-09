5b8f76211062e8dd4044285757fbc83917087573
Evan Jones <evanpeterjones@gmail.com>
2021-08-09 16:33:55 -0500

    first commit
    Next need to fix the File-Chooser errors so I can let Eric pick a directory.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to image-min-for-eric
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/image_min_for_eric/core_test.clj
@@ -0,0 +1,7 @@
+(ns image-min-for-eric.core-test
+  (:require [clojure.test :refer :all]
+            [image-min-for-eric.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
